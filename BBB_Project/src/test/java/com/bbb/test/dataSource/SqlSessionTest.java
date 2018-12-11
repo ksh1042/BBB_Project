4fc89e86a3a54d99606c62c858fa4f335224ab05
Adam <Adam@QB52CTB>
2018-12-11 21:59:10 +0900

    flag : dataSource-complete

--- BBB_Project/src/test/java/com/bbb/test/dataSource/SqlSessionTest.java
+++ BBB_Project/src/test/java/com/bbb/test/dataSource/SqlSessionTest.java
@@ -1,5 +1,31 @@
 package com.bbb.test.dataSource;
 
-public class SqlSessionTest {
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:com/bbb/context/dataSource-context.xml")
+@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=false)
+@Transactional
+public class SqlSessionTest {
+	
+	@Autowired
+	private SqlSession session;
+	
+	@Test
+	public void sqlSession_Test(){
+		Assert.assertThat(session, not( nullValue()));
+	}
 }
