d04ab6d8b06334e69ae637024719fc859d1736e5
skgus <skgus@DESKTOP-LHMA5IG>
2018-12-26 16:31:23 +0900

    modify2

--- BBB_Project/src/main/java/com/bbb/controller/MainController.java
+++ BBB_Project/src/main/java/com/bbb/controller/MainController.java
@@ -3,16 +3,22 @@ package com.bbb.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bbb.dto.MemberVO;
+import com.bbb.service.MemberService;
 
 @Controller
 @RequestMapping("/main")
 public class MainController {
 
+	@Autowired
+	private MemberService service;
+	
 	@RequestMapping(value="" ,method=RequestMethod.GET)
 	public String memberTurningPoint(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		MemberVO loginUser = (MemberVO)request.getSession().getAttribute("loginUser");
@@ -28,5 +34,10 @@ public class MainController {
 	@RequestMapping("/mainForm")
 	public String mainForm() throws Exception{
 		return "/main/main";
+	}
+	
+	@RequestMapping(value="/mypage/modify",method=RequestMethod.POST)
+	public void myPageModify(String id, Model model)throws Exception{
+		
 	}
 }
--- BBB_Project/src/main/java/com/bbb/controller/MemberController.java
+++ BBB_Project/src/main/java/com/bbb/controller/MemberController.java
@@ -2,10 +2,8 @@ package com.bbb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bbb.dto.MemberVO;
 import com.bbb.service.MemberService;
 
 @Controller
@@ -15,13 +13,7 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
-	@RequestMapping("/mypage")
-	public String myPageForm(String id, Model model)throws Exception{
-		MemberVO member=service.getMemberById(id);
-		model.addAttribute("member",member);
-		
-		return "member/mypage/detail";
-	}
+	
 	
 	
 }
