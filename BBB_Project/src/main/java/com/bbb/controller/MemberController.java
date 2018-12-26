bfb37865a3f032a335b5d839a455ae9216584d06
skgus <skgus@DESKTOP-LHMA5IG>
2018-12-26 09:25:00 +0900

    add : mypage detail

--- BBB_Project/src/main/java/com/bbb/controller/MemberController.java
+++ BBB_Project/src/main/java/com/bbb/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.bbb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bbb.dto.MemberVO;
 import com.bbb.service.MemberService;
 
 @Controller
@@ -12,6 +14,14 @@ public class MemberController {
 
 	@Autowired
 	private MemberService service;
+	
+	@RequestMapping("/mypage")
+	public String myPageForm(String id, Model model)throws Exception{
+		MemberVO member=service.getMemberById(id);
+		model.addAttribute("member",member);
+		
+		return "member/mypage/detail";
+	}
 	
 	
 }
