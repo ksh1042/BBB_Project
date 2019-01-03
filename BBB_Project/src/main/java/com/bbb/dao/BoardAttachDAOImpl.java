af0cf40b9c4ba49b5eb01cd005088ab259e35408
Adam <Adam@QB52CTB>
2019-01-03 11:15:14 +0900

    conflict merge

--- BBB_Project/src/main/java/com/bbb/dao/BoardAttachDAOImpl.java
+++ BBB_Project/src/main/java/com/bbb/dao/BoardAttachDAOImpl.java
@@ -8,19 +8,6 @@ import org.apache.ibatis.session.SqlSession;
 import com.bbb.dto.BoardAttachVO;
 
 public class BoardAttachDAOImpl implements BoardAttachDAO {
-<<<<<<< HEAD
-
-   private SqlSession session;
-   public void setSession(SqlSession session){
-      this.session=session;
-   }
-   
-   @Override
-   public void insertAttach(BoardAttachVO attach) throws SQLException {
-      session.update("Board.insertAttach",attach);
-
-   }
-=======
 	
 	private SqlSession session;
 	public void setSession(SqlSession session){
@@ -29,57 +16,26 @@ public class BoardAttachDAOImpl implements BoardAttachDAO {
 	
 	private final String namespace = "BoardAttachMapper";
 	
-	
-	
-	
 	@Override
 	public void insertAttach(BoardAttachVO attach) throws SQLException {
 		session.update(namespace+".insertAttach",attach);
 	}
->>>>>>> refs/remotes/origin/IMC
-
-<<<<<<< HEAD
-   @Override
-   public void deleteAttach(String uUid) throws SQLException {
-      session.update("Board.deleteAttach",uUid);
-
-   }
-=======
+	
 	@Override
 	public void deleteAttach(String uuid) throws SQLException {
 		session.update(namespace+".deleteAttach",uuid);
 	}
->>>>>>> refs/remotes/origin/IMC
 
-<<<<<<< HEAD
-   @Override
-   public List<BoardAttachVO> selectAttachesBybNum(int bNum) throws SQLException {
-      List<BoardAttachVO> attachList =
-            session.selectList("Board.selectAttachBybNum",bNum);
-      return attachList;
-   }
-=======
 	@Override
 	public List<BoardAttachVO> selectAttachesBybNum(int bNum) throws SQLException {
 		List<BoardAttachVO> attachList=
 				session.selectList(namespace+".selectAttachBybNum",bNum);
 		return attachList;
 	}
->>>>>>> refs/remotes/origin/IMC
-
-<<<<<<< HEAD
-   @Override
-   public void deleteAllAttach(int bNum) throws SQLException {
-      session.update("Board.deleteAllAttach", bNum);
-      
-   }
-   
-}
-=======
+	
 	@Override
 	public void deleteAllAttach(int bNum) throws SQLException {
 		session.update(namespace+".deleteAllAttach",bNum);
 	}
 
 }
->>>>>>> refs/remotes/origin/IMC
